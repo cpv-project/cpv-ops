54cfd7e1fe1a96b65921a5c08b72e7c69eb0886c
compiv <compiv@tutanota.com>
2019-06-26 21:19:37 -0400

    update devenv

--- local/scripts/devenv-build.sh
+++ local/scripts/devenv-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 . ./.config.sh
-docker build -t "${DEVENV_IMAGE_NAME}" ../docker/devenv
+docker build -t "${DEVENV_IMAGE_NAME}" ../docker/devenv --no-cache=true
 docker system prune -f
 
--- local/scripts/devenv-start.sh
+++ local/scripts/devenv-start.sh
@@ -2,14 +2,19 @@
 . ./.config.sh
 
 docker rm -f "${DEVENV_CONTAINER_NAME}" 2>/dev/null || true
+docker system prune -f
 
 docker run \
   --name "${DEVENV_CONTAINER_NAME}" \
   --volume /var/home/data/local:/var/home/data/local \
   --net host \
   --cap-add=SYS_PTRACE \
+  --cap-add=SYS_NICE \
+  --cap-add=SYS_ADMIN \
+  --cap-add=SYSLOG \
   --security-opt label=disable \
   --restart always \
+  -p 8000:8000 \
   -d \
   "${DEVENV_IMAGE_NAME}"
 
